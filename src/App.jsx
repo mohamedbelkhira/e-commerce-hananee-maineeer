d384ca2d081946fa4ee81a3b666821e9a006f400
Tokita <mohamedbelkhira09@gmail.com>
2024-07-31 21:52:00 +0100

    front-end is functionnal

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,13 @@ import Footer from './components/major/footer/Footer';
 import { CartProvider } from './context/CartContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import ScrollToTop from './components/common/ScrollToTop';
 const App = () => {
   return (
     <CartProvider>
       <div className="flex flex-col min-h-screen">
       <Navbar />
+      <ScrollToTop />
       <Router />
       <Footer />
       <ToastContainer />
